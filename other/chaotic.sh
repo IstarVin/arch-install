2a9d4373384439d293d6e1f0ce6586c3707135d1
IstarVin <ajdeleverio20@gmail.com>
2025-06-21 22:42:21 +0800

    update

---
+++ other/chaotic.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+# Check if running as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e "${RED}Error: This script must be run as root${NC}" >&2
+    exit 1
+fi
+
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+
+pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+tee -a /etc/pacman.conf > /dev/null <<-EOF
+	[chaotic-aur]
+	Include = /etc/pacman.d/chaotic-mirrorlist
+EOF
+
+pacman -Sy --noconfirm --needed yay
